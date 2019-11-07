d5039e8d4a353b684949184b6e1c162f9c982e8d
kimyongjin <yongjin5184@gmail.com>
2019-11-07 21:36:46 +0900

    Java In Action 5.5 연습문제 풀이 / 책 내용 정리
    1. 연습문제 풀잌
    2. 리듀싱 정의 추가

--- java8/src/streams/Java8InAction5_5.java
+++ java8/src/streams/Java8InAction5_5.java
@@ -2,6 +2,7 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
@@ -10,7 +11,7 @@ public class Java8InAction5_5 {
     private static List<Transaction> transactions;
     public static void main(String[] args) {
         init();
-        tr2011();
+        practice();
     }
 
     private static void init() {
@@ -29,13 +30,54 @@ public class Java8InAction5_5 {
         );
     }
 
-    private static void tr2011() {
-        /*1. 2011년에 일어난 모든 트랜잭션을 찾아서 값을 오름차순으로 정렬하시오.*/
+    private static void practice() {
+        /* 1. 2011년에 일어난 모든 트랜잭션을 찾아서 값을 오름차순으로 정렬하시오. */
         List<Transaction> tr2011 = transactions.stream()
-                .filter(transaction -> transaction.getYear() == 2011)
-                .sorted(comparing(Transaction::getValue))
-                .collect(toList());
-        System.out.println(tr2011);
+            .filter(transaction -> transaction.getYear() == 2011)
+            .sorted(comparing(Transaction::getValue))
+            .collect(toList());
+
+        /* 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오.*/
+        List<String> cities = transactions.stream()
+            .map(transaction -> transaction.getTrader().getCity())
+            .distinct()
+            .collect(toList());
+
+        /* 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하시오. */
+        List<Trader> traders = transactions.stream()
+            .map(transaction -> transaction.getTrader())
+            .filter(trader -> trader.getCity().equals("Cambridge"))
+            .distinct()
+            .sorted(comparing(Trader::getName))
+            .collect(toList());
+
+        /* 4. 모든 거래자의 이름을 알파벳순으로 정렬해서 반환하시오. */
+        String traderStr = transactions.stream()
+            .map(transaction -> transaction.getTrader().getName())
+            .distinct()
+            .sorted()
+            .reduce("", (n1, n2) -> n1 + n2);
+
+        /* 5. 밀라노에 거래자가 있는가?*/
+        boolean milanBased = transactions.stream()
+            .anyMatch(transaction -> transaction.getTrader()
+                                                .getCity()
+                                                .equals("Milan"));
+
+        /* 6.케임브리지에 거주하는 거래자의 모든 트랜잭션값을 출력하시오. */
+        transactions.stream()
+            .filter(t -> "Cambridge".equals(t.getTrader().getCity()))
+            .map(Transaction::getValue)
+            .forEach(System.out::println);
+
+        /* 7. 전체 트랜잭션 중 최댓값은 얼마인가? */
+        Optional<Integer> highestValue = transactions.stream()
+            .map(Transaction::getValue)
+            .reduce(Integer::max);
+
+        /* 8. 전체 트랜잭션 중 최소값(트랜잭션)은 얼마인가? */
+        Optional<Transaction> smallestTransaction = transactions.stream()
+            .reduce((t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2);
     }
 }
 
