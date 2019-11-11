f5a87939eff0bf86f74e26173437f4acbf640457
kimyongjin <yongjin5184@gmail.com>
2019-11-11 23:29:32 +0900

    디자인 패턴 추가
    1. Composite Pattern 추가

---
+++ designPattern/src/desgin/pattern/composite/Line.java
@@ -0,0 +1,8 @@
+package desgin.pattern.composite;
+
+public class Line implements Shape {
+    @Override
+    public void draw(String color) {
+        System.out.println("line color: " + color);
+    }
+}
---
+++ designPattern/src/desgin/pattern/composite/Shape.java
@@ -0,0 +1,5 @@
+package desgin.pattern.composite;
+
+public interface Shape {
+    public void draw(String color);
+}
