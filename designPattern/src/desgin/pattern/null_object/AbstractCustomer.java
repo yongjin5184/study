8ff8e8c11e45bd667b2871ddb3d4e0bbcbc8e385
kimyongjin <yongjin5184@gmail.com>
2020-01-12 23:22:03 +0900

    디자인 패턴
    1. Null object pattern 추가

---
+++ designPattern/src/desgin/pattern/null_object/AbstractCustomer.java
@@ -0,0 +1,7 @@
+package desgin.pattern.null_object;
+
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
